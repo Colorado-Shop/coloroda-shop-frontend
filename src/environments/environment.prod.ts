441b3937e4ceab2263112dff1b096658966d1922
Colorado-Shop <125808948+Colorado-Shop@users.noreply.github.com>
2023-04-25 20:04:04 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,8 @@ export const environment = {
   // API_URL: 'http://localhost:9000/api/v1',
 //   API_URL: 'https://colorado-shop-api.herokuapp.com/api/v1',
   API_URL: 'https://exortic-email.osc-fr1.scalingo.io/api/v1',
-  db_api_url: 'https://exotics-front.osc-fr1.scalingo.io/api/v1',
+//   db_api_url: 'https://exotics-front.osc-fr1.scalingo.io/api/v1',
+  
+  db_api_url: 'https://long-cyan-angler-wig.cyclic.app/api/v1',
+//   https://long-cyan-angler-wig.cyclic.app/api/v1/products
 }
