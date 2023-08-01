7e05f3f8040ee7f575ed9a9854b895dcce9fc626
high-green <highgreenzone@yandex.com>
2023-08-01 03:06:18 +0100

    resume working

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -12,6 +12,7 @@ import { ProductDBService } from 'src/app/services/product.db.service'
 export class CategoryComponent implements OnInit {
   products: Product[] = new Array()
   id: string
+  description: string = ""
 
   constructor(
     private productService: ProductService,
@@ -26,25 +27,32 @@ export class CategoryComponent implements OnInit {
           this.products = productData.products
           console.log(this.products)
         })
+
       // this.products = productService.findByCategory(this.id);
+      if (this.id == "strain") {
+        this.description = `Each cannabis strain has a different concentration of the cannabinoids tetrahydrocannabinol (THC), cannabidiol (CBD), as well as other compounds. Cultivators and farmers have long been breeding different varieties with certain characteristics to create hybrids and specific strains.`
+      } else if (this.id == "concentrate") {
+        this.description = `Cannabis concentrate, also called marijuana concentrate, marijuana extract, or cannabis extract, is a tetrahydrocannabinol (THC) and/or cannabidiol (CBD) concentrated mass. Cannabis concentrates contain high THC levels that range from 40% to over 90%,[1][2] stronger in THC content than high-grade marijuana, which normally measures around 20% THC levels.`
+      } else if (this.id == "seed") {
+        this.description = `Our comprehensive selection of feminized cannabis seeds has been expertly bred to produce only female cannabis plants. Female cannabis plants are guaranteed to provide cannabinoid-rich flowers for growers to harvest.`
+      } else if (this.id == "vape_pen") {
+        this.description = `
+        Vape Pens are extremely popular mainly due to their subtle, slim and discreet design. A perfect stepping stone between disposable vapes and advanced kits, vape pens provide the user with a hassle-free, accessible setup that can utilise a wide range of vape liquids. Powered by a rechargeable built-in battery, vape pens are activated with a press to the fire button which in turn heats the coil element and creates the vapour that is inhaled. 
+        `
+      }else if (this.id == "muha_meds") {
+        this.description = `
+        Our extraction team is dedicated to crafting premium THC oil. Made with cannabis derived terpenes to offer a balanced aroma and taste to deliver a consistent experience every time. \n
+        Get long-lasting hours while vaping. Our vape pens have rechargeable batteries with great battery life. We also make replacing the batteries easy and you have muhameds batteries for sale at the most affordable prices
+        `
+      } else if (this.id == "rove") {
+        this.description = `
+        Rove built the foundation of their company on honesty, simplicity, and transparency. Their mission is to provide customers with great tasting vape cartridges and reliable and dependable vape pens. Rove proudly stands opposed to the trend of exaggerated quality and misinformation within product test results. Rove wants their customers to know they have their best interests in mind with every single step they take. They strive for excellence and are dedicated to improvement.
+        Welcome to Rove, where the best is always getting better.
+        `
+      }
     })
 
-    enum mapDescription {
-      strain = `Each cannabis strain has a different concentration of the cannabinoids tetrahydrocannabinol (THC), cannabidiol (CBD), as well as other compounds. Cultivators and farmers have long been breeding different varieties with certain characteristics to create hybrids and specific strains.`,
-      concentrate = `Cannabis concentrate, also called marijuana concentrate, marijuana extract, or cannabis extract, is a tetrahydrocannabinol (THC) and/or cannabidiol (CBD) concentrated mass. Cannabis concentrates contain high THC levels that range from 40% to over 90%,[1][2] stronger in THC content than high-grade marijuana, which normally measures around 20% THC levels.`,
-      seed = `Our comprehensive selection of feminized cannabis seeds has been expertly bred to produce only female cannabis plants. Female cannabis plants are guaranteed to provide cannabinoid-rich flowers for growers to harvest.`,
-      vape_pen = `
-      Vape Pens are extremely popular mainly due to their subtle, slim and discreet design. A perfect stepping stone between disposable vapes and advanced kits, vape pens provide the user with a hassle-free, accessible setup that can utilise a wide range of vape liquids. Powered by a rechargeable built-in battery, vape pens are activated with a press to the fire button which in turn heats the coil element and creates the vapour that is inhaled. 
-      `,
-      muha_meds = `
-      Our extraction team is dedicated to crafting premium THC oil. Made with cannabis derived terpenes to offer a balanced aroma and taste to deliver a consistent experience every time. \n
-      Get long-lasting hours while vaping. Our vape pens have rechargeable batteries with great battery life. We also make replacing the batteries easy and you have muhameds batteries for sale at the most affordable prices
-      `,
-      rove = `
-      Rove built the foundation of their company on honesty, simplicity, and transparency. Their mission is to provide customers with great tasting vape cartridges and reliable and dependable vape pens. Rove proudly stands opposed to the trend of exaggerated quality and misinformation within product test results. Rove wants their customers to know they have their best interests in mind with every single step they take. They strive for excellence and are dedicated to improvement.
-      Welcome to Rove, where the best is always getting better.
-      `
-    }
+   
   }
 
   ngOnInit() {}
